9d7e05b79ca97b652a1f5f40ce5f8eae520f46a0
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-13 04:11:47 +0300

    commit tree 7db6cf58ad54f41a2c99597de07b318c449cc66d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+   
+    console.log(`This Saturday, I want to ${activity}!`)
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(par1="*") {
+    
+    return function(emphatic="special"){ 
+        return `You are ${par1}${emphatic}${par1}!` 
+     
+    }
+}
